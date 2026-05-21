29a1845ab352bcc438fb683abc7561773e9b1631
Szabó-Király Farkas <113937305+SzFarkas32@users.noreply.github.com>
2026-05-21 14:37:28 +0200

    Add files via upload

---
+++ Szabó-Király Farkas/Methods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirintus
+{
+    internal class Methods
+    {
+
+        /// <summary>
+        /// Megadja, hogy hány termet tartamaz a térkép
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>Termek száma</returns>
+        static int GetRoomNumber(char[,] map)
+        {
+            int roomNumber = 0;
+            for(int x = 0; x < map.GetLength(0); x++)
+            {
+                for(int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (map[x, y] == '█') 
+                    {
+                        roomNumber++;
+                    }
+                }
+            }
+            return roomNumber;
+        }
+        /// <summary>
+        /// A kapott térkép széleit végignézve megállapítja, hogy hány kijárat van.
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>Az alkalmas kijáratok száma</returns>
+        static int GetSuitableEntrance(char[,] map)
+        {
+            return -1;
+        }
+        /// <summary>
+        /// Megnézi, hogy van-e a térképen meg nem engedett karakter?
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>true - A térkép tartalmaz szabálytalan karaktert, false - nincs benne ilyen</returns>
+        static bool IsInvalidElement(char[,] map)
+        {
+            return true;
+        }
+        /// <summary>
+        /// Visszaadja azoknak a járatkaraktereknek a pozícióját, amelyekhez egyetlen szomszéd pozícióból sem lehet eljutni.
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>A pozíciók "sor_index:oszlop_index" formátumban szerepelnek a lista elemeiként
+        static List<string> GetUnavailableElements(char[,] map)
+        {
+            List<string> unavailables = new List<string>();
+            // ?
+            // pld: string poz = "4:12"; 
+            return unavailables;
+        }
+        /// <summary>
+        /// Labiritust generál a kapott pozíciókat tartalmazó lista alapján. A lista elemei egymáshoz kapcsolódó járatok pozíciói.
+        /// </summary>
+        /// <param name="positionsList">"sor_index:oszlop_index" formátumban az egymáshoz kapcsolódó járatok pozícióit tartalmazó lista </param>
+        /// <returns>A létrehozott labirintus térképe</returns>
+        static char[,] GenerateLabyrinth(List<string> positionsList)
+        {
+            return null;
+        }
+    }
+}
