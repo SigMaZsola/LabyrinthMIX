b2ba28dd58566a93c17f763223ae6affd86fa984
matyibobo <116632311+matyibobo@users.noreply.github.com>
2026-05-25 14:44:16 +0200

    Utak kiválasztása
    + a UI-ban apró módosítások

--- Mészár Máté/MapEditor/MainWindow.xaml.cs
+++ Mészár Máté/MapEditor/MainWindow.xaml.cs
@@ -16,9 +16,33 @@ namespace MapEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        char[] symbols = ['═', '╬', '╦', '╩', '║', '╣', '╠', '╗', '╝', '╚', '╔'];
+
         public MainWindow()
         {
             InitializeComponent();
+
+            txtCurrent.Text = symbols[0].ToString();
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                grdPaths.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                Button btn = new Button();
+                btn.Content = symbols[i];
+                btn.FontSize = 30;
+                btn.Click += ChangeSymbol;
+                Grid.SetColumn(btn, i);
+                grdPaths.Children.Add(btn);
+            }
+        }
+
+        private void ChangeSymbol(object sender, RoutedEventArgs e)
+        {
+            txtCurrent.Text = sender.ToString().Substring(sender.ToString().Length - 1);
         }
     }
 }
